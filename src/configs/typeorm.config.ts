eeee0a9be93a1efaa8bfac8b34f97037efd5e58e
sebiinim <1219sebin@gmail.com>
2024-03-04 14:03:58 +0900

    env test

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'postgres',
-    host: '34.47.92.148',
-    port: 5432,
-    username: 'root',
-    password: '1234',
-    database: 'SCBESVDB',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,  
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
     autoLoadEntities: true
